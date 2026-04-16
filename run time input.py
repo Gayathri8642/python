400d4cb07249864a30e567dad0c48964cf1e3da2
Gayathri8642 <134222875+Gayathri8642@users.noreply.github.com>
2026-04-16 17:54:16 +0530

    Add files via upload

---
+++ run time input.py
@@ -0,0 +1,65 @@
+'''a=4
+b=8
+print(a+b)'''
+
+'''a=7
+b=95
+print(a+b)'''
+#print(4+6)
+#runtime input
+'''a=int(input('a value'))
+b=int(input('b value'))
+print(a+b)'''
+
+'''a=float(input('a value'))
+b=float(input('b value'))
+print(a+b)'''
+
+'''a=str(input('a value'))
+b=str(input('b value'))
+print(a+b)'''
+
+'''a=input('a value')
+b=input('b value')
+print(a+b)'''
+
+'''fname=input('fname')
+lname=input('lname')
+print(fname+''+lname)'''
+
+'''city=input('city')
+print('city is ',city)'''
+
+'''a=bool(input('a value'))
+b=bool(input('b value'))
+print(a+b)'''
+
+'''a=complex(input('a value'))
+b=complex(input('b value'))
+print(a+b)'''
+
+'''a=int(input('a value'))
+b=int(input('b value'))
+option=int(input('choose the option 1.add 2.sub 3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input('a value'))
+b=int(input('b value'))
+option=int(input('choose the option
+                  1.add
+                  2.sub
+                  3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=input('a value')
+b=input('b value')
+option=input('choose the option add sub mul)
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
