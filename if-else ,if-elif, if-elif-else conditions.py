7354929e225e698ddc81d64aa98b1fe7a8d2eeff
Gayathri8642 <134222875+Gayathri8642@users.noreply.github.com>
2026-04-18 18:01:37 +0530

    Add files via upload

---
+++ if-else ,if-elif, if-elif-else conditions.py
@@ -0,0 +1,210 @@
+#if else conditions by using comparision
+'''a=5
+b=10
+if a<b:
+    print("less")
+else:
+    print("true")'''
+
+
+'''a=4
+b=6
+if a>b:
+    print("less")
+else:
+    print("true")'''
+
+
+'''a=5
+b=5
+if a!=b:
+    print("not equal")
+else:
+    print("true")'''
+
+'''a=10
+b=10
+if a==b:
+    print("equal")
+else:
+    print("true")'''
+
+
+#if else conditions by using logic
+
+'''a=9
+b=12
+if a<b and b>a:
+    print("True")
+else :
+    print("False")'''
+
+'''a=9
+b=12
+if a!=b and b==a:
+    print("True")
+else :
+    print("False")'''
+
+'''a=15
+b=18
+if not a<b and b>a:
+    print("True")
+else :
+    print("False")'''
+
+#if else conditions by using identity
+
+'''a=9
+if type(a) is int:
+    print("true")
+else:
+    print("false")'''
+
+'''a=9
+if type(a) is not int:
+    print("true")
+else:
+    print("false")'''
+
+#if else conditions by using membership
+
+'''a=[10,20,30,40,50]
+if 10 in a:
+    print("true")
+else :
+    print("false")'''
+
+'''a=[10,20,30,40,50]
+if 10 not in a:
+    print("true")
+else :
+    print("false")'''
+
+#if-elif conditions by using comparision
+
+'''a=2
+b=4
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")'''
+
+'''a=2
+b=4
+if a==b:
+    print("less")
+elif b>a:
+    print("greater")'''
+
+'''a=6
+b=12
+if a==b:
+    print("equal")
+elif b>a:
+    print("b is greater")
+elif a>b:
+    print("a is greater")'''
+        
+#if-elif conditions by using logical
+
+'''a=6
+b=12
+if a<b and b>a:
+    print("less")
+elif b>a or a!=b:
+    print("greater")
+elif not a==b or a<b:
+    print("true")'''
+
+#if-elif conditions by using membership
+
+'''a=[45,56,95,65,75]
+if 46 in a:
+    print("true")
+elif 46 not in a:
+    print("false")'''
+
+
+#if-elif conditions by using identity
+
+'''a=9
+if type(a) is int:
+    print("true")
+elif type(a) is not int:
+    print("false")'''
+
+#if-elif-else conditions by using comparision
+'''a=8
+b=9
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+elif a!=b:
+    print("not equal")
+else:
+    print("true")'''
+
+#if-elif-else conditions by using logical
+
+'''a=8
+b=9
+if a<b and b<a:
+    print("less")
+elif a!=b or a>b:
+    print("not equal")
+else:
+    print("true")'''
+
+
+#if-elif-else conditions by using identity
+
+'''a=20
+if 20 is a:
+    print("it is same as a")
+elif 20 is not a:
+    print("it is not same as a")
+else :
+    print("not compared")'''
+    
+
+#if-elif-else conditions by using membership
+'''a=[10,20,30,40,50,60,70,80]
+if 20 in a:
+    print("20 is in a")
+elif 20 not in a:
+    print("20is not in a")
+else :
+    print("value")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
