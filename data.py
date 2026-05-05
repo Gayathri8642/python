587cf7cdd0b8560d80079ecc779a471007651ec4
Gayathri8642 <134222875+Gayathri8642@users.noreply.github.com>
2026-05-05 17:30:04 +0530

    modules and data

---
+++ data.py
@@ -0,0 +1,19 @@
+'''import modules
+modules.g('gayathri')'''
+
+#import modules
+
+#import modules
+
+
+
+'''import modules
+a=modules.d["num"]
+b=modules.d["names"]
+print(a)
+print(b)
+
+for i in modules.d:
+    print(i)'''
+
+import modules
---
+++ modules.py
@@ -0,0 +1,26 @@
+'''def g(n):
+    print('welcome',n)'''
+
+'''a=7
+b=6
+c=a+b
+print(a+b)'''
+
+'''a=input('f')
+b=input('l')
+print(a+b)'''
+
+'''d={"num":[10,20,30],'names':['gayathri','kusuma','dhatri']}'''
+
+'''if __name__=="__main__":
+    a=[10,20,30,40]
+    a.extend("code")
+    print(a)# this program run as module because we are not importing any function'''
+
+def d():
+    if __name__=="__main__":
+        print('script')
+    else:
+        print('module')
+d()
+    
