948cb94779a746a865bbe65cf5493c92cd6d097c
Gayathri8642 <134222875+Gayathri8642@users.noreply.github.com>
2026-05-14 16:31:42 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,20 @@
+#oops syntax
+'''class classname:
+    n='gayathri'
+    p='taduvai'
+    y='2026'
+    def fname(method_name):
+        print("statements..................")
+a=classname()
+a.fname()'''
+
+#class declaration
+class details():
+    n='gayathri'
+    p='taduvai'
+    y=2026
+    def fname(se):
+        print(se.n,se.p,se.y)
+a=details()
+print(dir(a))
+a.fname()
